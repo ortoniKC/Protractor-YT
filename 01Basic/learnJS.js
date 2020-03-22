e66080eddf529992b4dae2497c71136d943f4880
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-03-22 19:24:47 +0530

    Add files via upload

---
+++ 01Basic/learnJS.js
@@ -0,0 +1,9 @@
+// console.log(124)
+
+
+let name = 'koushik';
+// console.log("My name is "+name);
+console.log(`My name is ${name}`);
+
+console.log("hey i'm koushik");
+
---
+++ 01Basic/letVsVar.js
@@ -0,0 +1,21 @@
+// const panNumber = "ABCD123"
+// console.log(panNumber);
+// panNumber = "xyz123"
+// console.log(panNumber);
+
+
+// var name = "koushik"
+// name = "chatterjee"
+// console.log(name);
+
+function counter(){
+// let i =100;
+    for(var i =0; i < 5; i++){
+        console.log(i);
+        
+    }
+    console.log(i);
+    
+}
+counter()
+console.log(i);
