91c8e6ed19b19da18793e62673e41bb88c64dba1
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-03-24 00:32:52 +0530

    Add files via upload

---
+++ 01Basic/learnFunctions.js
@@ -0,0 +1,40 @@
+
+() =>{
+    console.log("logic");
+}
+
+
+// // add()
+// function add(a, b) {
+//     // logic
+//     console.log(a + b);
+
+// }
+// function add(a, b, c) {
+//     // logic
+//     console.log(a + b + c);
+
+// }
+// add(3, 2)
+
+
+
+
+// let addition = function(){
+//     console.log(5+2);
+
+// }
+// addition()
+
+
+// let add = function(num1, num2){
+// console.log(num1+num2);
+// }
+// add(1,2)
+
+
+// let add = () => {
+//     console.log(1+2);
+// }
+// add()
+
