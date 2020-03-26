c0bd788f3b7ef1c2c2ccc54c73adae740542bc71
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-03-26 21:23:59 +0530

    Add files via upload

---
+++ 01Basic/learnString.js
@@ -0,0 +1,50 @@
+let str = 'Welcome to Letcode'
+console.log(str);
+
+console.log(str.toUpperCase());
+console.log(str);
+str = "hey";
+console.log(str);
+
+
+let len = str.length;
+console.log(len);
+
+console.log(str.charAt(len-1));
+
+console.log(str.substring(0, 7));
+
+console.log(str.replace('Letcode', 'protractor'));
+
+
+let name = new String("koushik")
+console.log(name);
+
+console.log(typeof str);
+console.log(typeof name);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str.includes('t'));
+
+console.log(str.split(" "));
+let words = str.split(' ')
+words.forEach(ele =>{
+    console.log(ele);
+})
+console.log(str.length);
+
+let space = " hey welcome "
+console.log(space.trimRight());
+
+
+
+
+
+
+
+
+
+
+
