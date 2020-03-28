61102ba879fc85c82ebe60e782b33351e600fa98
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-03-29 01:53:08 +0530

    Add files via upload

---
+++ 01Basic/closure.js
@@ -0,0 +1,24 @@
+function getAge(){ // outer
+    let age = 0; //new born baby
+    // return ++age;
+    return ()=>{ //inner
+        return ++age;
+    }
+}
+let a = getAge()
+console.log(getAge()());
+console.log(getAge()());
+
+// console.log(a());
+// console.log(a());
+// console.log(a());
+
+
+
+
+
+
+// console.log(getAge());
+// // --age;
+// console.log(getAge());
+
