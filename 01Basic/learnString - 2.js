f5595b18c74bfd97fa06299a0c6531787d3e98ce
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-03-28 19:38:13 +0530

    Add files via upload

---
+++ 01Basic/learnString - 2.js
@@ -0,0 +1,27 @@
+let now = 'Stay home, Stay safe'
+
+console.log(now.includes('h'));
+
+// console.log(now.startsWith('Stay'));
+// console.log(now.endsWith('e'));
+
+
+// let firstName = 'koushik'
+// let lastName = ' Chatterjee'
+// console.log(firstName.concat(lastName));
+// console.log(firstName);
+// let fullName = firstName.concat(lastName);
+// console.log(fullName);
+
+
+// let one = "apple" // literal
+// let two = new String("apple") //object
+// console.log(one == two);
+
+// let num = 1; // number
+// let numStr = "1"; // string
+// console.log( num == numStr);
+// console.log(num === numStr);
+// let a = "1";
+// let b = "10"
+// console.log(a+b);
