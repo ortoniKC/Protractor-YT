5cb0bc1dc3ac6df94aa150bdd4d744fb8a4cbc1b
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-04-01 17:36:56 +0530

    Add files via upload

---
+++ 01Basic/dataTypes.js
@@ -0,0 +1,61 @@
+// String
+let name = 'koushik'
+console.log(name);
+console.log(typeof name);
+
+// Number
+let num = 0
+console.log(num);
+console.log(typeof num);
+
+let weight = 65.0
+console.log(typeof weight);
+
+// Boolean
+let b = false
+console.log(typeof b);
+
+// Undefined
+let a
+console.log(a);
+
+// Null
+let c = ''
+console.log(typeof c);
+c = null;
+console.log(typeof c);
+
+
+
+// Object
+// let browser = {
+//     "name": "chrome",
+//     "version": 80,
+//     "vendor": "Google",
+//     fun: () => {
+//         console.log('I eat RAM');
+//     }
+// }
+// console.log(typeof browser);
+// console.log(browser);
+// browser.fun();
+// console.log(browser.name);
+
+let browsers = [
+    {
+        "name": "chrome",
+        "version": 80
+    },
+    {
+        "name": "firefox",
+        "version": 75
+    }
+]
+console.log(browsers[1].name);
+console.table(browsers)
+
+
+
+
+
+
