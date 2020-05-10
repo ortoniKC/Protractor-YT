77943f241701f00c5c1a89aedd37ab0b32ddddf3
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-05-10 11:55:51 +0530

    https://youtu.be/u3M6Ofm839c
    https://youtu.be/u3M6Ofm839c

---
+++ interactWithButtons/buttons.spec.js
@@ -0,0 +1,29 @@
+import { element, by, browser, $ } from "protractor";
+
+describe('Interact with buttons', () => {
+    beforeAll(async () => {
+        await browser.get('https://letcode.in/button')
+    })
+    it('Protractor specific locator', async () => {
+        let text = await element(by.buttonText('Goto Home')).getText()
+        console.log(text);
+    });
+
+    it('Get Position', async () => {
+        // element(by.id())
+        let location = await $('#button2').getLocation()
+        console.log(location.x);
+        console.log(location.y);
+    });
+
+    it('Button color', async () => {
+        let color = await $('.btn.btn-primary').getCssValue('background-color')
+        console.log(color);
+    });
+
+    it('Find the height and width', async () => {
+        let size = await element(by.buttonText('Submit Button')).getSize()
+        console.log(size.height);
+        console.log(size.width);
+    });
+})
