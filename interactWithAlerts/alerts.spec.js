e2a5846ea68898e0c58ccad25dbcaf2a51bde8ae
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-05-20 00:05:04 +0530

    alerts

---
+++ interactWithAlerts/alerts.spec.js
@@ -0,0 +1,35 @@
+import { element, by, browser, $ } from "protractor";
+
+describe('Interact with Alert', () => {
+    beforeAll(async () => {
+        await browser.get('https://letcode.in/alert')
+    })
+
+    fit('Simple Alert', async () => {
+        await element(by.buttonText("Simple")).click();
+        // console.log(await browser.getTitle());
+        await browser.sleep(3000);
+        (await browser.switchTo().alert()).accept()
+    });
+
+    it('Confirm Alert', async () => {
+        await element(by.buttonText('Confirm')).click();
+        await browser.sleep(3000);
+        console.log(await (await browser.switchTo().alert()).getText());
+        (await browser.switchTo().alert()).dismiss();
+    });
+
+    it('Prompt Alert', async () => {
+        await element(by.buttonText('Prompt')).click()
+        await browser.sleep(3000);
+        let alert = await browser.switchTo().alert();
+        await alert.sendKeys("koushik");
+        await alert.accept();
+    });
+
+    it('Sweet Alert', async () => {
+        await element(by.buttonText('Sweet')).click();
+        await browser.sleep(2000)
+        await element(by.buttonText('OK')).click();
+    });
+})
