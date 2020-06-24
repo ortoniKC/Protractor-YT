30c4578a4e3a267ed58279f25cdc0c00c35f3df0
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-06-24 21:53:40 +0530

    Add files via upload

---
+++ learnMap/learnElementAll.js
@@ -0,0 +1,28 @@
+const { browser, $$, $ } = require("protractor");
+
+describe("Learn element.all - MAP", () => {
+
+    beforeAll(async () => {
+        await browser.waitForAngularEnabled(false);
+        await browser.manage().timeouts().implicitlyWait(20000);
+        await browser.get("https://erail.in/trains-between-stations/mgr-chennai-ctr-MAS/ksr-bengaluru-SBC")
+    })
+
+    it("Print train numbers", async () => {
+        await $("#chkSelectDateOnly").click();
+        await browser.sleep(3000);
+        let rows = $$("#divTrainsList .TrainList tr");
+        let myTrainNumberMap = await rows.map(
+            async (ele, index) => {
+                let firstData = ele.$$('td').get(0);
+                return {
+                    "trainNumber": await firstData.getText(),
+                    "index": index
+                }
+            }
+        );
+        console.log(await myTrainNumberMap);
+
+    })
+
+});
