a7b2df378fae77f31890c710fa2f4f9bdd235b80
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-07-12 17:42:30 +0530

    Add files via upload

---
+++ webtable/webtable.js
@@ -0,0 +1,26 @@
+const { browser, element, $ } = require("protractor")
+
+describe('lets learn WebTable', () => {
+
+
+    beforeAll(async () => {
+        await browser.get("https://play.letcode.in/table");
+    })
+    it('table', async () => {
+        let table = $("table#table tbody");
+        let rows = table.$$("tr");
+        let count = await rows.count();
+        console.log(count);
+        expect(count).toBe(3);
+        for (let i = 0; i < count; i++) {
+            let firstTD = rows.get(i).$$("td");
+            let firstData = firstTD.get(0);
+            let text = await firstData.getText();
+            if (text === "Yashwanth") {
+                let checkBox = firstTD.last().$("input");
+                await checkBox.click();
+            }
+        }
+        await browser.sleep(5000);
+    })
+})
