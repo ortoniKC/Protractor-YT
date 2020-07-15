36071cf7b8b40fd141f7aabf63ce4fc1b1bb16e9
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-07-16 00:32:31 +0530

    https://youtu.be/FavXQLVy4w4

---
+++ actions/dragdrop.spec.js
@@ -0,0 +1,21 @@
+const { browser, element, by } = require("protractor")
+
+describe("Actions", () => {
+
+    beforeAll(async () => {
+        await browser.get("https://play.letcode.in/aui");
+    })
+
+    it("Drag & Drop", async () => {
+        let source = element(by.id("draggable"));
+        let target = element(by.id("droppable"));
+        await browser.actions().dragAndDrop(await source.getWebElement(),
+            await target.getWebElement())
+            .perform();
+    })
+
+
+    afterAll(async () => {
+        await browser.sleep(5000);
+    })
+})
